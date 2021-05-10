bbebd57916ab0189a9fd6dd4b9bebeb4cdd58076
Marcos Lazaro Alvarez Arteaga <marcosalv92@gmail.com>
2021-05-10 19:42:46 -0400

    Proyecto fetch terminado

--- src/index.js
+++ src/index.js
@@ -3,4 +3,48 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseURL = 'https://platzi-avo.vercel.app';
+const API = `${baseURL}/api/avo`
+
+const getData = async () =>{
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data);
+    const allItems = [];
+    data.data.forEach(element => {
+        console.log(element);
+        // crear imagen 
+        const imagen = document.createElement('img');
+        imagen.src = `${baseURL}${element.image}`;
+        
+        
+        //crear titulo
+        const titulo = document.createElement('h2');
+        titulo.textContent = element.name;
+
+        //crear  precio
+        const precio = document.createElement('div');
+        precio.textContent = element.price;
+        
+        const container = document.createElement('div');
+        container.append(imagen,titulo,precio);
+        allItems.push(container);
+        
+
+    });
+    
+    const container = document.querySelector('#container');
+    container.append(...allItems);
+    
+    return allItems;
+}
+
+const allItems = getData();
+
+// allItems.forEach((element)=>{
+//     element.addEventListener('onclick',updateClick)
+
+// })
+// const updateClick = () => {
+//     console.log('dio un click');
+// }
